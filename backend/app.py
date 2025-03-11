d0f8ecf054b10359a1633a739c2cec8b93d73f53
Mitali <mitalichoudhary710@gmail.com>
2025-03-11 21:00:25 +0530

    Initial commit: set up Geospatial Project

--- backend/app.py
+++ backend/app.py
@@ -1,24 +1,21 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
+from flask import Flask, send_from_directory
 import requests
 
 app = Flask(__name__)
-CORS(app)
 
-# Replace with your AQICN API token
-API_TOKEN = "dd54d0ecebff369e45f9ff135004c150617233df"
+# Serve QGIS-processed GeoJSON data
+@app.route('/aqi-data', methods=['GET'])
+def get_aqi_data():
+    return send_from_directory('data', 'aqi_heatmap.geojson')
 
-@app.route('/')
-def home():
-    return "GEOCLOUD4AQI Backend is Running!"
-
-
+# Fetch real-time AQI data
 @app.route('/aqi/<city>', methods=['GET'])
 def get_aqi(city):
+    API_TOKEN = "dd54d0ecebff369e45f9ff135004c150617233df"
     url = f"https://api.waqi.info/feed/{city}/?token={API_TOKEN}"
     response = requests.get(url)
     data = response.json()
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
